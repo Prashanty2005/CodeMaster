cd3261b22231333ff33869ac9d6cd43ab21a1db1
Prashanty2005 <prashantyanpallewar172@gmail.com>
2026-03-21 19:49:08 +0530

    unnecessary imports deleted

--- FrontEnd/src/components/ActivityHeatmap.jsx
+++ FrontEnd/src/components/ActivityHeatmap.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import axiosClient from '../utils/axiosClient';
 
 const ActivityHeatmap = () => {
